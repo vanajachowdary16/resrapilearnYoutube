5b2fd9a27af630649da98c6899527703a480ac00
vanajachowdary16 <vanajachowdary12@gmail.com>
2025-09-24 11:11:19 +0530

    demo api

--- src/test/java/response/UserProfileResponse.java
+++ src/test/java/response/UserProfileResponse.java
@@ -2,11 +2,70 @@ package response;
 
 public class UserProfileResponse {
 	
+	public UserProfileResponse() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "UserProfileResponse [id=" + id + ", username=" + username + ", email=" + email + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", mobileNumber=" + mobileNumber + "]";
+	}
+
 	int id;
 	String username;
 	String email;
 	String firstName;
 	String lastName;
 	String mobileNumber;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
+	public UserProfileResponse(int id, String username, String email, String firstName, String lastName,
+			String mobileNumber) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.mobileNumber = mobileNumber;
+	}
+	
 
 }
